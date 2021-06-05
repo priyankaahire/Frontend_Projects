d0e33ba05636dc3070af7782d95f5c6c1dcf9109
priyankaahire <psahire01@gmail.com>
2021-06-05 22:17:36 +0530

    Remove dead code

--- Day8_CarMoving/index.js
+++ Day8_CarMoving/index.js
@@ -1,32 +1,10 @@
 const surface = document.querySelector('.surface');
-const car = document.querySelector('.car');
-
-const night = document.querySelector('.night');
-
 //keypress event
-
-
-//window.onload = function() {
-    // document.getElementById("night").onkeypress = runScript;
-    document.addEventListener("keypress", function(event) {
-        // Number 13 is the "Enter" key on the keyboard
-        console.log(event);
-        if (event.keyCode === 13 || event.keyCode === 32) {
-          // Cancel the default action, if needed
-          event.preventDefault();
-          surface.classList.toggle("moveRight");
-       
-          // Trigger the button element with a click
-         // document.getElementById("myBtn").click();
-        }
-      });
-//}
-//   function runScript(e) {
-//       alert("data");
-//     //See notes about 'which' and 'key'
-//     if (e.keyCode == 13) {
-//         var tb = document.getElementById("scriptBox");
-//         eval(tb.value);
-//         return false;
-//     }
-// }
+document.addEventListener("keypress", function(event) {
+    // Number 13 is the "Enter" key & 32 is the spacebar on the keyboard
+    if (event.keyCode === 13 || event.keyCode === 32) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      surface.classList.toggle("moveRight");
+    }
+});
